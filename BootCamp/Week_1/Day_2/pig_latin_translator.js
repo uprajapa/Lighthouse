34238fe05553fc53e693c3cf3b37d14ef1ccd19d
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-17 22:00:49 -0400

    pig latin added

--- BootCamp/Week_1/Day_2/pig_latin_translator.js
+++ BootCamp/Week_1/Day_2/pig_latin_translator.js
@@ -3,7 +3,7 @@ let originalWords = process.argv.slice(2);
 const translateToPigLatin = function(word) {
   let pigLatinWords = "";
   for (let letter of word) {
-    pigLatinWords = pigLatinWords === "" ? "" : `${pigLatinWords} `
+    pigLatinWords = pigLatinWords === "" ? "" : `${pigLatinWords} `;
     pigLatinWords += letter.slice(1) + letter[0] + "ay";
   }
   return pigLatinWords;
