561a5deaeef4b5192f628224a4b2b804c2789195
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-19 19:04:30 -0400

    mapping added

---
+++ BootCamp/Week_1/functions/map_builtin/mapTest.js
@@ -0,0 +1,5 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+let lengthOfLighthouses = lighthouses.map((lighthouse) => lighthouse.length);
+
+console.log(`Output:  ${lengthOfLighthouses}`);
