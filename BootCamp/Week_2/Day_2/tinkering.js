ac93ec66fa89f6eb8679867d3d0c2c800fd1c50e
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-30 14:30:11 -0400

    loan updated

--- BootCamp/Week_2/Day_2/tinkering.js
+++ BootCamp/Week_2/Day_2/tinkering.js
@@ -1,5 +1,5 @@
 const jsonString = '{"a":1, "b":2, "foo":"bar"}'; // string version of a JS Object
-const obj = JSON.parse(jsonString) // a string, of course :)
+const obj = JSON.parse(jsonString); // a string, of course :)
 delete obj.foo;
 const newString = JSON.stringify(obj);
 console.log(newString);
---
+++ BootCamp/Week_2/async/loan.js
@@ -0,0 +1,33 @@
+let creditLimit = 5000;
+
+/*
+ * Input: number of dollars to loan out
+ * Returns: Promise of loan which may or may not fulfill, based on remaining credit.
+ * If creditLimit is less than the requested amount, only the remaining limit is loaned out, otherwise the full amount is loaned out. If $0 remain in the limit, the loan request is rejected (error!).
+ */
+const loanOut = function(amount) {
+  return new Promise((resolve, reject) => {
+    let loanedOutAmount = 0;
+    if (creditLimit === 0) {
+      return reject(`Error: not enough credit limit!`);
+    }
+    if (creditLimit < amount) {
+      loanedOutAmount = amount - creditLimit;
+      creditLimit = 0;
+
+    } else {
+      loanedOutAmount = amount;
+      creditLimit -= amount;
+    }
+    return resolve(loanedOutAmount);
+  });
+};
+
+console.log("Asking for $150, which should be okay ...");
+loanOut(150)
+  .then((amountReceived) => {
+    console.log(`\t-> I got $${amountReceived} loan from the bank! Remaining Credit Limit: $${creditLimit}`);
+  })
+  .catch((err) => {
+    console.log(`\t-> Error: ${err}!`);
+  });
